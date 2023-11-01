b45db1e114b432e24f8e418122135aa4a8fe396d
pavlina-baeva <leevesnovich03@gmail.com>
2023-11-01 16:50:56 +0300

    made function's logic

---
+++ app.js
@@ -0,0 +1,46 @@
+const task = document.querySelector('.to-do__input');
+const list = [];
+
+//add new task to the list
+function addTask (text, list) {
+    /*const timestamp = Date.now();
+    const newTask = {
+        id:timestamp,
+        text,
+        isComplete:false
+    }
+    if (valudateInput) {
+    list.push(newTask);
+    }*/
+}
+
+//check input for being full & not repeating previous task
+function validateTask (text, list) {
+    /*const taskText = task.value;
+    if (taskText == '') {
+        добавить класс error со стилизацией инпута и сообщением об ошибки
+    } 
+
+    list.forEach((task) => {
+        if (taskText == text) {
+            alert('Такая задача уже существует');
+
+        }
+    })
+    */
+}
+
+//change task's style when it's done
+function completeTask () {
+
+}
+
+//delete task from the list 
+function deleteTask () {
+
+}
+
+//delete all tasks from the list
+function deleteTaskList () {
+
+}
