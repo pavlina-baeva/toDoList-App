1297b249a76e06665890620af37a20aa79794708
pavlina-baeva <leevesnovich03@gmail.com>
2023-11-01 17:54:14 +0300

    add error & taskList render functions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const task = document.querySelector('.to-do__input');
-const list = [];
+const taskList = [];
 
 //add new task to the list
 function addTask (text, list) {
@@ -9,26 +9,59 @@ function addTask (text, list) {
         text,
         isComplete:false
     }
-    /*
-    if (valudateInput) {
+    taskList.push(newTask);
+    renderTaskList(taskList);
+    /*if (valudateInput(newTask, taskList)) {
     list.push(newTask);
     }*/
 }
 
+function createError(input, text) {
+    const parent = input.parentNode;
+    const errorElement = document.createElement('label');
+    errorElement.className = 'errorMessage';
+    errorElement.textContent = text;
+    input.classList.add('error');
+    parent.append(errorElement);
+}
+
+function removeError(input) {
+    const parent = input.parentNode;
+    if (input.classList.contains('error')) {
+        parent.querySelector('.errorMessage').remove();
+        input.classList.remove('error');
+    }
+}
+
 //check input for being full & not repeating previous task
 function validateTask (text, list) {
-    /*const taskText = task.value;
+    /*let 
+    const taskText = task.value;
     if (taskText == '') {
-        добавить класс error со стилизацией инпута и сообщением об ошибки
-    } 
+        createError(task, 'Напишите задачу!');
+    } else {
+        removeError(task);
+    }
 
-    list.forEach((task) => {
-        if (taskText == text) {
+    list.forEach((newTask) => {
+        if (newTask.text == text) {
             alert('Такая задача уже существует');
 
         }
     })
     */
+}
+
+function renderTaskList(list) {
+    list.forEach(task => {
+        const taskHTML = `
+        <div id='${newTask.id}' class='${}'>
+        <input type="checkbox" ${checked}><div class = "to-do__checkbox-div"></div>
+        <div class="to-do__taskText">${task.text}</div>
+        <button class="to-do__deleteBtn">Удалить</button>
+        </div>
+        `
+    })
 }
 
 //change task's style when it's done
