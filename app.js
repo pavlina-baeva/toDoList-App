d3fb477b689b8b2f69143a3da491c13a36957e3f
pavlina-baeva <leevesnovich03@gmail.com>
2023-11-05 20:42:45 +0300

    rewrite event function witout mistakes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const task = document.querySelector('.to-do__input');
 const errorMessage = document.querySelector('.errorMessage');
+const listElement = document.querySelector('.to-do__list');
+const addBtn = document.querySelector('.to-do__add-btn');
 const taskList = [];
+
+//click for adding task
+addBtn.onclick = () => {
+    const taskText = task.value;;
+    if (validateTask(taskText)) {
+        addTask(taskText, taskList);
+        task.value = '';
+        renderTaskList(taskList);
+    }
+}
 
 //add new task to the list
 function addTask (text, list) {
@@ -10,11 +22,7 @@ function addTask (text, list) {
         text,
         isComplete:false
     }
-    taskList.push(newTask);
-    renderTaskList(taskList);
-    /*if (valudateInput(newTask, taskList)) {
     list.push(newTask);
-    }*/
 }
 
 //error functions for input-validation
@@ -33,9 +41,8 @@ function removeError(input) {
 
 
 //check input for being full & not repeating previous tasks
-function validateTask (text, list) {
-    const taskText = task.value;
-    if (taskText == '') {
+function validateTask (text) {
+    if (text == '') {
         createError(task, 'Напишите задачу!');
         return false;
     } else {
@@ -52,17 +59,20 @@ function validateTask (text, list) {
 }
 
 function renderTaskList(list) {
+    let listHTML ='';
     list.forEach(task => {
-        let listHTML ='';
         //const cls = newTask.isComplete ? ;
+        const checked = task.isComplete ? 'checked' : '';
         const taskHTML = `
         <div id='${newTask.id}' class='${cls}'>
         <input type="checkbox" ${checked}><div class = "to-do__checkbox-div"></div>
         <div class="to-do__taskText">${task.text}</div>
-        <button class="to-do__deleteBtn">Удалить</button>
+        <button class="to-do__delBtn">Удалить</button>
         </div>
         `
+        listHTML = listHTML + taskHTML;
     })
+    listElement.innerHTML = listHTML;
 }
 
 //change task's style when it's done
