3f80d4883f67c4c309e983ab52970e598b8508cd
pavlina-baeva <leevesnovich03@gmail.com>
2023-11-06 23:28:14 +0300

    made function renderTaskList( ) work

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const taskList = [];
 //click for adding task
 addBtn.onclick = () => {
     const taskText = task.value;;
-    if (validateTask(taskText)) {
+    if (validateTask(taskText) === true) {
         addTask(taskText, taskList);
         task.value = '';
         renderTaskList(taskList);
@@ -39,33 +39,33 @@ function removeError(input) {
     }
 }
 
-
 //check input for being full & not repeating previous tasks
 function validateTask (text) {
+    let isValid = true;
     if (text == '') {
         createError(task, 'Напишите задачу!');
-        return false;
+        isValid = false;
     } else {
         removeError(task);
+        isValid = true;
     }
-
+    return isValid;
     /*list.forEach((newTask) => {
         if (newTask.text == text) {
             alert('Такая задача уже существует');
-
         }
     })
     */
 }
 
+//function to renderi tasklist
 function renderTaskList(list) {
-    let listHTML ='';
+    let listHTML = '';
     list.forEach(task => {
-        //const cls = newTask.isComplete ? ;
-        const checked = task.isComplete ? 'checked' : '';
+        //const cls = newTask.isComplete ? 'to-do__newTask to-do__completed' : 'to-do__newTask';
         const taskHTML = `
-        <div id='${newTask.id}' class='${cls}'>
-        <input type="checkbox" ${checked}><div class = "to-do__checkbox-div"></div>
+        <div id='${task.id}' class='to-do__newTask'>
+        <input type="checkbox"><div class = "to-do__checkbox-div"></div>
         <div class="to-do__taskText">${task.text}</div>
         <button class="to-do__delBtn">Удалить</button>
         </div>
